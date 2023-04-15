06cb5912355fa55427db3af3500f34fe424d0fe2
fukashigi1 <maatiaasbkn2@gmail.com>
2023-04-15 19:08:32 -0400

    d

--- main.py
+++ main.py
@@ -1,52 +1,70 @@
 import random
+import pandas as pd
+
 class ia2():
     def __init__(self):
-        self.network = {1: 2, 2: 3, 3: 3, 4: 2} #Red neuronal Clave=Capa Valor=Cantidad Neuronas.
+        self.db = pd.read_excel("Libro1.xlsx")
+
+        self.network = {1: 3, 2: 3, 3: 3, 4: 2} #Red neuronal Clave=Capa Valor=Cantidad Neuronas.
         self.weightLayer = {} #Iniciar diccionario de capas de pesos.
         self.biases = {} #Diccionario de umbrales.
-        self.XS = [] #Base de datos con X y S. Si los dos son positivos dará 1, de lo contrario dará 0.
-
-        self.batch = 2
+        self.forwardPropagationResults = {} #Resultado de las salidas por iteración {i: yi}
+        self.newValueNeurons = {} #Nuevos resultados de las neuronas con las funciones de activación.
+        self.batch = 5 #Lote
+        self.doneBatch = {}
 
         self.weightGenerator()
         self.biasGenerator()
-        self.dataBaseGenerator()
-        self.inputValuesGenerator()
+        self.forwardPropagation()
 
     def weightGenerator(self):
         for layer in range(1, len(self.network)):
             self.weightLayer[layer] = []
             weightsInLayer = self.network[layer] * self.network[layer+1]
-            for ws in range(1, weightsInLayer+1):
-                self.weightLayer[layer].append(random.random())
+            for weights in range(1, weightsInLayer+1):
+                self.weightLayer[layer].append([round(random.random(), 8)])
+            pointer = 0
+            for i in range(1, self.network[layer] + 1):
+                for j in range(1, self.network[layer+1] + 1):
+                        self.weightLayer[layer][pointer].append(i)
+                        self.weightLayer[layer][pointer].append(j)
+                        pointer = pointer +1
 
     def biasGenerator(self):
         for layer in self.network:
             self.biases[layer] = []
             if layer == 1:
-                pass
+                for neuron in range(1, self.network[layer]+1):
+                    self.biases[layer].append([neuron, "No posee"])
             else:
                 for neuron in range(1, self.network[layer]+1):
-                    self.biases[layer].append(random.random())
+                    self.biases[layer].append([neuron, round(random.random(), 8)])
+        
+    def forwardPropagation(self):
+        def transformValue(value, column):
+            return round(((value - self.db[column].min()) / (self.db[column].max() - self.db[column].min())), 8)
+        
+        totalBatch = 0
 
-    def dataBaseGenerator(self):
-        for i in range (0, 20):
-            row = []
-            for neuron in range(1, self.network[1]+1):
-                key = random.randint(-100, 100)
-                row.append(key)
-            if all(value > 0 for value in row):
-                val = 1
-            else:
-                val = 0
-            row.append(val)
-            self.XS.append(row)
-    
-    def inputValuesGenerator(self, iteration):
-        for z in range(0, iteration):
-            for neurons in range(1, self.network[1]+1):
-                pass 
-            #el entrenamiento en lotes acumula los gradientes de error para un conjunto de datos completo y luego realiza una actualización de pesos única basada en esos gradientes acumulados.
-
+        for batches in range(0, int(self.db.shape[0] / self.batch)+1):
+            print(f'Batch número {batches} ->> Datos recorridos {totalBatch}')
+            self.doneBatch[batches] = {}
+            for neuronLayer in self.biases:
+                self.newValueNeurons[neuronLayer] = []
+                if neuronLayer == 1: #Verificar que sea la capa 1 para asignar los valores de entrada.
+                    for index in range(totalBatch, self.batch):
+                        pointer = 0
+                        for column in self.db.columns[:len(self.biases[1])]:
+                            value = self.db.iloc[index][column]
+                            self.newValueNeurons[neuronLayer].append([pointer+1, transformValue(value, str(column))])
+                            pointer +=1
+                            print(self.newValueNeurons)
+                        totalBatch += 1
+                        
+                else:
+                    for neuron in self.biases[neuronLayer]:
+                        print(neuron)
+        print(self.doneBatch)
+        
 if __name__ == "__main__":
     ia2()
